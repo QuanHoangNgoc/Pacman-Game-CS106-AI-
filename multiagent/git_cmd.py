159566a6389610adf45d05fb374bca1daeed03fa
QuanHoangNgoc <22521178@gm.uit.edu.vn>
2024-04-16 10:51:58 +0700

    [percent(99)_version]

--- multiagent/git_cmd.py
+++ multiagent/git_cmd.py
@@ -6,6 +6,13 @@ git add .
 git commit -m "[percent(99)_version]" 
 git branch -M main 
 git remote add origin https://github.com/QuanHoangNgoc/Pacman_Game.git 
-git push -u origin main 
 git push --force origin main
+
+git init
+git status
+git add . 
+git commit -m "[percent(99)_version]" 
+git branch -M main 
+git remote add origin https://github.com/QuanHoangNgoc/Pacman_Game.git 
+git push -u origin main 
 '''
